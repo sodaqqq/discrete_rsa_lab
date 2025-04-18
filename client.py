6cc2c79c1f8c79ba16324336ebdbcfb2780a6c4c
Ostap Kostiuk <164146945+sodaqqq@users.noreply.github.com>
2025-04-18 20:59:08 +0300

    code refactored now everething looks good and works well

--- client.py
+++ client.py
@@ -1,97 +1,63 @@
 import socket
 import threading
 import hashlib
-from rsa import generate_key_pair, encode, decode, PublicKey, PrivateKey, MessageLengthError
-
+from rsa import generate_key_pair, encode, decode, PublicKey, MessageLengthError
 
 class Client:
     def __init__(self, server_ip: str, port: int, username: str) -> None:
         self.server_ip = server_ip
         self.port = port
         self.username = username
-        self.secret_key = None
 
     def init_connection(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            self.s.connect((self.server_ip, self.port))
-        except Exception as e:
-            print("[client]: could not connect to server: ", e)
-            return
-
+        self.s.connect((self.server_ip, self.port))
         self.s.send(self.username.encode())
 
-        # create key pairs
-        self.private_key, self.public_key = generate_key_pair()
-
-        # exchange public keys
-        public_key_bytes = str(self.public_key.n).encode() + b'|' + str(self.public_key.e).encode()
-        self.s.send(public_key_bytes)
-
-        # receive the encrypted secret key
-        server_public_key_data = self.s.recv(1024).decode()
-        n_str, e_str = server_public_key_data.split('|')
-        self.server_public_key = PublicKey(n=int(n_str), e=int(e_str))
-
-        encrypted_secret = self.s.recv(1024)
-        encrypted_secret_int = int.from_bytes(encrypted_secret, 'big')
-        decrypted_secret_int = decode(encrypted_secret_int, self.private_key)
-        self.secret_key = decrypted_secret_int.to_bytes((decrypted_secret_int.bit_length()+7)//8, 'big')
+        pub = self.s.recv(1024).decode()
+        n_str, e_str = pub.split("|")
+        self.server_pub = PublicKey(int(n_str), int(e_str))
+        self.priv_key, self.pub_key = generate_key_pair()
+        pub_to_send = f"{self.pub_key.n}|{self.pub_key.e}".encode()
+        self.s.send(pub_to_send)
 
         message_handler = threading.Thread(target=self.read_handler,args=())
         message_handler.start()
         input_handler = threading.Thread(target=self.write_handler,args=())
         input_handler.start()
 
-    def read_handler(self): 
+    def read_handler(self):
         while True:
-            message = self.s.recv(1024)
+            data = self.s.recv(8192)
+            if not data:
+                break
 
-            if len(message) < 32:
-                raise Exception('given message is to short')
+            hash = data[:32]
+            encrypted = data[32:]
+            decrypted_int = decode(int.from_bytes(encrypted, 'big'), self.priv_key)
+            decrypted_bytes = decrypted_int.to_bytes((decrypted_int.bit_length() + 7) // 8, 'big')
+            message = decrypted_bytes.decode()
 
-            message_hash = message[:32]
-            encrypted_message_bytes = message[32:]
-
-            encrypted_message_int = int.from_bytes(encrypted_message_bytes, 'big')
-
-            try:
-                decrypted_message_int = decode(encrypted_message_int, self.private_key)
-                decrypted_bytes = decrypted_message_int.to_bytes((decrypted_message_int.bit_length() + 7) // 8, 'big')
-                decrypted_message = decrypted_bytes.decode()
-            except Exception as e:
-                print("failed to decode message:", e)
-                continue
-            
-            hash = hashlib.sha256(decrypted_message.encode()).digest()
-
-            if hash == message_hash:
-                print(f"{decrypted_message}")
+            if hashlib.sha256(message.encode()).digest() == hash:
+                print(message)
             else:
-                print("message was damaged")
+                print("integrity check failed :(")
+
 
     def write_handler(self):
         while True:
             message = input()
 
-            message_hash = self.get_hash(message)
-            message_bytes = message.encode()
-            message_int = int.from_bytes(message_bytes, 'big')
-
             try:
-                encrypted_int = encode(message_int, self.server_public_key)
+                message_bytes = message.encode()
+                message_hash = hashlib.sha256(message_bytes).digest()
+                message_int = int.from_bytes(message_bytes, 'big')
+                encrypted_int = encode(message_int, self.server_pub)
+                encrypted_bytes = encrypted_int.to_bytes((encrypted_int.bit_length() + 7) // 8, 'big')
+                message_final = message_hash + encrypted_bytes
+                self.s.send(message_final)
             except MessageLengthError:
-                print("message too long for encryption")
-                continue
-
-            encrypted_bytes = encrypted_int.to_bytes((encrypted_int.bit_length() + 7) // 8, 'big')
-            final_message = message_hash + encrypted_bytes
-
-            self.s.send(final_message)
-    
-    def get_hash(self, message):
-        return hashlib.sha256(message.encode()).digest()
+                print("message too long for RSA")
 
 if __name__ == "__main__":
-    cl = Client("127.0.0.1", 9001, "b_g")
-    cl.init_connection()
+    Client("127.0.0.1", 9001, "b_g").init_connection()
--- server.py
+++ server.py
@@ -1,80 +1,79 @@
 import socket
 import threading
 import hashlib
-import os
-from rsa import generate_key_pair, encode, decode, PublicKey, PrivateKey
-
+from rsa import generate_key_pair, encode, decode, PublicKey, MessageLengthError
 
 class Server:
     def __init__(self, port: int) -> None:
         self.host = "127.0.0.1"
         self.port = port
-        self.clients = []
-        self.username_lookup = {}
-        self.client_public_keys = {}
-        self.secret_keys = {}
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.usernames = {}
+        self.public_keys = {}
         self.private_key, self.public_key = generate_key_pair()
 
     def start(self):
         self.s.bind((self.host, self.port))
         self.s.listen(100)
+        print(f"listening port {self.port}")
 
         while True:
             c, addr = self.s.accept()
-            username = c.recv(1024).decode()
-            print(f"{username} tries to connect")
-            self.broadcast(f"new person has joined: {username}")
-            self.username_lookup[c] = username
-            self.clients.append(c)
+            threading.Thread(target=self.handle_client, args=(c,)).start()
 
-            public_key_bytes = f"{self.public_key.n}|{self.public_key.e}".encode()
-            c.send(public_key_bytes)
+    def handle_client(self, c: socket.socket):
+        username = c.recv(1024).decode()
+        self.usernames[c] = username
+        self.clients.append(c)
+        print(f"{username} connected")
 
-            client_public_key_bytes = c.recv(1024).decode()
-            n_str, e_str = client_public_key_bytes.split("|")
-            client_public_key = PublicKey(int(n_str), int(e_str))
-            self.client_public_keys[c] = client_public_key
+        server_pub = f"{self.public_key.n}|{self.public_key.e}".encode()
+        c.send(server_pub)
 
-            secret_key = os.urandom(16)
-            self.secret_keys[c] = secret_key
+        client_pub = c.recv(1024).decode()
+        n_str, e_str = client_pub.split("|")
+        client_public_key = PublicKey(n=int(n_str), e=int(e_str))
+        self.public_keys[c] = client_public_key
 
-            secret_int = int.from_bytes(secret_key, 'big')
-            encrypted_secret = encode(secret_int, client_public_key)
-
-            encrypted_bytes = encrypted_secret.to_bytes((encrypted_secret.bit_length()+7)//8, 'big')
-            c.send(encrypted_bytes)
-
-            threading.Thread(
-                target=self.handle_client,
-                args=(
-                    c,
-                    addr,
-                ),
-            ).start()
-
-    def broadcast(self, msg: str):
-        for client in self.clients:
-            final_message = self.create_message(msg)
-            client.send(final_message)
-
-    def handle_client(self, c: socket.socket, addr):
         while True:
-            msg = c.recv(1024)
 
-            for client in self.clients:
-                if client != c:
-                    client.send(msg)
+            data = c.recv(8192)
+            if not data:
+                break
 
-    def create_message(self, message):
-        hash = self.get_hash(message)
-        return hash + message.encode()
+            hash = data[:32]
+            encrypted = data[32:]
 
-    def get_hash(self, message):
-        return hashlib.sha256(message.encode()).digest()
+            decrypted_int = decode(int.from_bytes(encrypted, 'big'), self.private_key)
+            decrypted_bytes = decrypted_int.to_bytes((decrypted_int.bit_length() + 7) // 8, 'big')
+            decrypted_message = decrypted_bytes.decode()
 
+            if hashlib.sha256(decrypted_message.encode()).digest() != hash:
+                print(f"failed integrity check")
+                continue
+
+            print(f"[{username}]: {decrypted_message}")
+            self.broadcast(decrypted_message)
+
+        self.clients.remove(c)
+        del self.usernames[c]
+        del self.public_keys[c]
+        c.close()
+
+    def broadcast(self, message: str):
+        for client in self.clients:
+            try:
+                pubkey = self.public_keys[client]
+                message_bytes = message.encode()
+                hash = hashlib.sha256(message_bytes).digest()
+                message_int = int.from_bytes(message_bytes, 'big')
+                encrypted_int = encode(message_int, pubkey)
+                encrypted_bytes = encrypted_int.to_bytes((encrypted_int.bit_length() + 7) // 8, 'big')
+                message_final = hash + encrypted_bytes
+                client.send(message_final)
+            except Exception:
+                print(f"failed to send message")
 
 if __name__ == "__main__":
-    s = Server(9001)
-    s.start()
-
+    Server(9001).start()
